f3d4970eb50d8598894e71e001e09f097ad52a0e
Sercan.PC <se-sercan@outlook.com>
2017-11-06 23:07:13 +0300

    addUserController Some basic structures Added

--- src/sample/addUserController.java
+++ src/sample/addUserController.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import javax.xml.soap.Text;
+
 public class addUserController {
+
+    @FXML
+    private TextField personNameField;
+    @FXML
+    private TextField personSurnameField;
+    @FXML
+    private TextField personEMailField;
+    @FXML
+    private TextField personAddressField;
+    @FXML
+    private TextField telephoneNumberField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField userPasswordField;
+
+
+
 }
