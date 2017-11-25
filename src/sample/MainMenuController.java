d4d5c2d88b4f31328387d9142997df7b11fb5273
Sercan.PC <se-sercan@outlook.com>
2017-11-25 13:50:59 +0300

    Main Menu Controller implemented methods
    ----New Features---
    
    *Main Menu Controller Class Methods Filled
    *Other Screens FXML files added and filled template

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -2,8 +2,13 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainMenuController {
 
@@ -17,53 +22,93 @@ public class MainMenuController {
     }
 
     @FXML
-    public void userActivities(ActionEvent event){
-
+    public void userActivities(ActionEvent event) throws Exception{
+        Parent userActivitiesScreen = FXMLLoader.load(getClass().getResource("UserActivitiesScreen.fxml"));
+        Scene userScene = new Scene(userActivitiesScreen);
+        Stage userStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        userStage.setScene(userScene);
+        userStage.show();
     }
 
     @FXML
-    public void newUser(ActionEvent event) {
-
+    public void newUser(ActionEvent event) throws Exception{
+        Parent newUserScreen = FXMLLoader.load(getClass().getResource("NewUserScreen.fxml"));
+        Scene newUserScene = new Scene(newUserScreen);
+        Stage newUserStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        newUserStage.setScene(newUserScene);
+        newUserStage.show();
     }
 
     @FXML
-    public void deleteUser(ActionEvent event) {
-
+    public void deleteUser(ActionEvent event) throws Exception{
+        Parent deleteUserScreen = FXMLLoader.load(getClass().getResource("DeleteUserScreen.fxml"));
+        Scene deleteUserScene = new Scene(deleteUserScreen);
+        Stage deleteUSerStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        deleteUSerStage.setScene(deleteUserScene);
+        deleteUSerStage.show();
     }
 
     @FXML
-    public void vehicleDatabase(ActionEvent event) {
-
+    public void vehicleDatabase(ActionEvent event) throws Exception{
+        Parent vehicleDatabaseScreen = FXMLLoader.load(getClass().getResource("VehicleDatabaseScreen.fxml"));
+        Scene vehicleDatabaseScene = new Scene(vehicleDatabaseScreen);
+        Stage vehicleDatabaseStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        vehicleDatabaseStage.setScene(vehicleDatabaseScene);
+        vehicleDatabaseStage.show();
     }
 
     @FXML
-    public void newVehicle(ActionEvent event) {
-
+    public void newVehicle(ActionEvent event) throws Exception{
+        Parent newVehicleScreen = FXMLLoader.load(getClass().getResource("NewVehicleScreen.fxml"));
+        Scene newVehicleScene = new Scene(newVehicleScreen);
+        Stage newVehicleStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        newVehicleStage.setScene(newVehicleScene);
+        newVehicleStage.show();
     }
 
     @FXML
-    public void deleteVehicle(ActionEvent event) {
-
+    public void deleteVehicle(ActionEvent event) throws Exception{
+        Parent deleteVehicleScreen = FXMLLoader.load(getClass().getResource("DeleteVehicleScreen.fxml"));
+        Scene deleteVehicleScene = new Scene(deleteVehicleScreen);
+        Stage deleteVehicleStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        deleteVehicleStage.setScene(deleteVehicleScene);
+        deleteVehicleStage.show();
     }
 
     @FXML
-    public void rentVehicle(ActionEvent event) {
-
+    public void rentVehicle(ActionEvent event) throws Exception{
+        Parent rentVehicleScreen = FXMLLoader.load(getClass().getResource("RentVehicleScreen.fxml"));
+        Scene rentVehicleScene = new Scene(rentVehicleScreen);
+        Stage rentVehicleStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        rentVehicleStage.setScene(rentVehicleScene);
+        rentVehicleStage.show();
     }
 
     @FXML
-    public void rentHistory(ActionEvent event) {
-
+    public void rentHistory(ActionEvent event) throws Exception{
+        Parent rentHistoryScreen = FXMLLoader.load(getClass().getResource("RentHistoryScreen.fxml"));
+        Scene rentHistoryScene = new Scene(rentHistoryScreen);
+        Stage rentHistoryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        rentHistoryStage.setScene(rentHistoryScene);
+        rentHistoryStage.show();
     }
 
     @FXML
-    public void vehicleOffDuty(ActionEvent event) {
-
+    public void vehicleOffDuty(ActionEvent event) throws Exception{
+        Parent vehicleOffDutyScreen = FXMLLoader.load(getClass().getResource("VehicleOffDutyScreen.fxml"));
+        Scene vehicleOffDutyScene = new Scene(vehicleOffDutyScreen);
+        Stage vehicleOffDutyStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        vehicleOffDutyStage.setScene(vehicleOffDutyScene);
+        vehicleOffDutyStage.show();
     }
 
     @FXML
-    public void customerHistory(ActionEvent event) {
-
+    public void customerHistory(ActionEvent event) throws Exception {
+        Parent customerHistoryScreen = FXMLLoader.load(getClass().getResource("CustomerHistoryScreen.fxml"));
+        Scene customerHistoryScene = new Scene(customerHistoryScreen);
+        Stage customerHistoryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        customerHistoryStage.setScene(customerHistoryScene);
+        customerHistoryStage.show();
     }
 
 
