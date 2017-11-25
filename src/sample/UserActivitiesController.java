e4564f816f7696f14a2b62a1b8635ccbb31f2e67
Sercan.PC <se-sercan@outlook.com>
2017-11-25 16:04:17 +0300

    Basic Methods overwritten all classes
    ----New Features---
    
    *Controller classes added
    *MainMenu.fxml buttons are active now

--- src/sample/UserActivitiesController.java
+++ src/sample/UserActivitiesController.java
@@ -1,4 +1,62 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class UserActivitiesController extends MainMenuController {
+    @Override
+    public void menuItemAbout(ActionEvent event) {
+        super.menuItemAbout(event);
+    }
+
+    @Override
+    public void deleteUser(ActionEvent event) throws Exception {
+        super.deleteUser(event);
+    }
+
+    @Override
+    public void deleteVehicle(ActionEvent event) throws Exception {
+        super.deleteVehicle(event);
+    }
+
+    @Override
+    public void customerHistory(ActionEvent event) throws Exception {
+        super.customerHistory(event);
+    }
+
+    @Override
+    public void newUser(ActionEvent event) throws Exception {
+        super.newUser(event);
+    }
+
+    @Override
+    public void newVehicle(ActionEvent event) throws Exception {
+        super.newVehicle(event);
+    }
+
+    @Override
+    public void vehicleDatabase(ActionEvent event) throws Exception {
+        super.vehicleDatabase(event);
+    }
+
+    @Override
+    public void rentHistory(ActionEvent event) throws Exception {
+        super.rentHistory(event);
+    }
+
+    @Override
+    public void rentVehicle(ActionEvent event) throws Exception {
+        super.rentVehicle(event);
+    }
+
+    @Override
+    public void userActivities(ActionEvent event) throws Exception {
+        super.userActivities(event);
+    }
+
+    @Override
+    public void vehicleOffDuty(ActionEvent event) throws Exception {
+        super.vehicleOffDuty(event);
+    }
+
 }
