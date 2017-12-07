32eac429994d9a1cf9f77bdc41c2ef89363c6945
macbookproretina <se-sercan@outlook.com>
2017-12-07 11:13:48 +0300

    Customer.java
    ---Change Log---
    *New Customer Class Added.

---
+++ src/sample/Customer.java
@@ -0,0 +1,60 @@
+package sample;
+
+
+import java.util.ArrayList;
+
+public class Customer implements Comparable<Customer> {
+    private String name;
+    private String surname;
+    private String address;
+    private String telephoneNumber;
+    private String idNumber;
+
+    Customer(String name , String surname, String address , String telephoneNumber , String idNumber){
+        setName(name);
+        setSurname(surname);
+        setAddress(address);
+        setTelephoneNumber(telephoneNumber);
+        setIdNumber(idNumber);
+        info.add(this);
+    }
+
+    public static ArrayList<Customer> info = new ArrayList<Customer>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    @Override
+    public int compareTo(Customer o) {
+        return 0;
+    }
+}
