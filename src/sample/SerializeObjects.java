caac5f104770ef384676d2c663c1d5837b8d4a17
Sercan.PC <se-sercan@outlook.com>
2017-12-17 18:13:06 +0300

    Vehicle object read and write methods of SerializeObject.java implemented

--- src/sample/SerializeObjects.java
+++ src/sample/SerializeObjects.java
@@ -17,7 +17,7 @@ public class SerializeObjects implements Serializable {
         for(int counter=0;counter<Personal.info.size();counter++){
             objectOutputStream.writeObject(Personal.info.get(counter));
         }
-        System.out.println("All objects written in file successfully");
+        System.out.println("All personal objects written in file successfully");
         objectOutputStream.close();
         fileOutputStream.close();
     }
@@ -47,7 +47,46 @@ public class SerializeObjects implements Serializable {
                 Personal.personalObservableList.add(Personal.info.get(counter));
             }
             System.out.println("All Personals Recorded Successful");
+            file.close();
         }
     }
+    public static void initializeVehicleObjectsToFile() throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("Vehicle.data");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        for (int counter=0;counter<Vehicle.info.size();counter++){
+            objectOutputStream.writeObject(Vehicle.info.get(counter));
+        }
+        System.out.println("All vehicle objects written in file successfully");
+        objectOutputStream.close();
+        fileOutputStream.close();
+    }
+    public static void initializeVehicleObjectsFromFile() throws IOException {
+        FileInputStream file = new FileInputStream("Vehicle.data");
+        ObjectInputStream objectInputStream = new ObjectInputStream(file);
+        boolean eof = true;
+        try {
+            while (eof) {
+                Vehicle obj = (Vehicle) objectInputStream.readObject();
+                if (obj != null) {
+                    Vehicle.info.add(obj);
+                    System.out.println("Object written");
 
+                } else {
+                    eof = false;
+                    System.out.println("end of file");
+                }
+            }
+        } catch (EOFException eofex) {
+            objectInputStream.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("class not found");
+            e.printStackTrace();
+        } finally {
+            for (int counter = 0; counter < Personal.info.size(); counter++) {
+                Vehicle.vehicleObservableList.add(Vehicle.info.get(counter));
+            }
+            System.out.println("All Vehicles Recorded Successful");
+            file.close();
+        }
+    }
 }
